4b995b1316f05d65d9f24d4d7dafa902d8f7896d
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-14 11:38:29 -0800

    Create 26. Remove Duplicates from Sorted Array.R

---
+++ 26. Remove Duplicates from Sorted Array.R
@@ -0,0 +1,27 @@
+removeduplicates <- function(nums){
+  if(length(nums)<=1){
+    return(c(length(nums),nums))
+  }
+  pt = 1
+  k=0
+  len=length(nums)
+  while(pt<len & (!is.na(nums[pt+1]))){
+    if(nums[pt] == nums[pt+1]){
+      val=nums[pt]
+      maxps=max(which(nums == val))
+      if(maxps<len){
+        nums[(pt+1):(len-(maxps-pt))]=nums[(maxps+1):len]
+        nums[((len-(maxps-pt))+1):len]=NA
+      }else{
+        nums[(pt+1):len]=NA
+      }
+    }
+    k = k + 1
+    pt = pt + 1
+  }
+  return(c(k+1, nums))
+}
+
+nums=c(0,0,1,1,1,2,2,3,3,4)
+removeduplicates(nums)
+

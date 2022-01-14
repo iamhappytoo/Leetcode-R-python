ee7a00cb56841779f032eb88b443f1d2d12a8853
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-14 11:51:25 -0800

    Update 27.Remove Element.R

--- 27.Remove Element.R
+++ 27.Remove Element.R
@@ -27,4 +27,20 @@ val=2
 
 remove(nums,val)
 
-
+###Another way, shrink array size and take advantage of the arbitrary order thing. 
+remove <- function(nums, val){
+  i = 1
+  n = length(nums)
+  while(i<n){
+    if(nums[i] == val){
+      nums[i] = nums[n]
+      n = n-1
+    }else{
+      i = i+1 
+    }
+  }
+  return(n)
+}
+nums=c(0,1,2,2,3,0,4,2)
+val=2
+remove(nums,val)

d5954a7c83c06078157180c911618b73febf9586
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-23 14:45:23 -0800

    Create Day4_New_Year_Chaos.py

---
+++ hankerrank/Day4_New_Year_Chaos.py
@@ -0,0 +1,53 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumBribes' function below.
+#
+# The function accepts INTEGER_ARRAY q as parameter.
+#
+
+def minimumBribes(q):
+    # Write your code here
+    Sumbribe = 0
+    for i, P in enumerate(q):
+        if P-1 > i:
+            if P-i-1 >2:
+                print("Too chaotic")
+                return 
+                
+#
+        # From here on out, we don't care if P has moved
+        # forwards, it is better to count how many times
+        # P has RECEIVED a bribe, by looking at who is
+        # ahead of P.  P's original position is the value
+        # of P.
+        # Anyone who bribed P (thus goes to in front of 
+        # P's current position cannot get to higher than
+        # one position in front if P's original position,
+        # so we need to look from one position in front
+        # of P's original position to one in front of P's
+        # current position, and see how many of those 
+        # positions in Q contain a number large than P.
+        # In other words we will look from P-1 to i-1,
+        # which in Python is range(P-1,i-1+1), or simply
+        # range(P-1,i).  To make sure we don't try an
+        # index less than zero, replace P-1 with
+        # max(P-1,0)
+        Sumbribe += sum(x>P for x in q[max(0,(P-2)):i])
+    print(Sumbribe)
+        
+if __name__ == '__main__':
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)

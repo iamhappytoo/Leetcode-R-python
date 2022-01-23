87e379dfde8a923a84c038379c5ae6c02b060e9b
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-23 12:33:34 -0800

    Create Day4_Grid_Challenge.py

---
+++ hankerrank/Day4_Grid_Challenge.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    for i,string in enumerate(grid):
+        grid[i]=sorted(string)
+    R = C = len(grid[0])
+    for c in range(C):
+        for r in range(1,R):
+            if ord(grid[r][c]) < ord(grid[r-1][c]):
+                return("NO")
+    return("YES")
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()

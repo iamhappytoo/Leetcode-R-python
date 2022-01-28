f6daf3e22d12e8dd3b94a996237d3961476c3f9f
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-28 10:50:53 -0800

    Update 104.Maximum_depth_of_binary_tree.py

--- 104.Maximum_depth_of_binary_tree.py
+++ 104.Maximum_depth_of_binary_tree.py
@@ -16,3 +16,12 @@ class Solution:
             if temp.right:
                 queue.append([temp.right, depth + 1])
         return depth
+##Another way is to do recursion (DFS)
+class Solution:
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+        else:
+            left_height = self.maxDepth(root.left)
+            right_height = self.maxDepth(root.right)
+            return max(left_height, right_height) + 1

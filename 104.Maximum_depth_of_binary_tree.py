38f98482946bbecef78e494f8f2f2742db142325
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-28 10:49:09 -0800

    Create 104.Maximum_depth_of_binary_tree.py

---
+++ 104.Maximum_depth_of_binary_tree.py
@@ -0,0 +1,18 @@
+class TreeNode:
+    def __init__(self):
+        self.data = None
+        self.left = None
+        self.right = None
+class Solution:
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+        queue = []
+        queue.append([root,1])
+        while queue:
+            temp, depth = queue.pop(0)
+            if temp.left:
+                queue.append([temp.left, depth + 1])
+            if temp.right:
+                queue.append([temp.right, depth + 1])
+        return depth

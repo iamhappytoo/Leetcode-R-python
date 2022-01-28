0d1398d27199990a916c28b399b6a6ae4c14bc2a
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-27 19:20:52 -0800

    Update 24.Swap_Node_in_Pair.py

--- 24.Swap_Node_in_Pair.py
+++ 24.Swap_Node_in_Pair.py
@@ -20,3 +20,45 @@ class Solution:
             curr = temp
             
         return savehead
+###Using a easier to understand denotation
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)
+        dummy.next = head
+        prev_node = dummy
+        while head and head.next:
+            first_node = head
+            second_node = head.next
+            
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+            
+            prev_node = first_node
+            head = first_node.next
+            
+        return dummy.next
+###Using a recursive way
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        first_node = head
+        second_node = head.next
+         
+        first_node.next = self.swapPairs(second_node.next)
+        second_node.next = first_node
+        
+        return second_node
+        

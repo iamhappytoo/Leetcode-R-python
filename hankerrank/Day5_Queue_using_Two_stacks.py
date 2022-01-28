c29912cead33887443c7b7118ccc2af0b276d566
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-28 10:39:51 -0800

    Update Day5_Queue_using_Two_stacks.py

--- hankerrank/Day5_Queue_using_Two_stacks.py
+++ hankerrank/Day5_Queue_using_Two_stacks.py
@@ -10,3 +10,39 @@ for i in range(int(input())):
         old.pop()
     else:            
         print(old[-1] if old else new[0])
+
+        
+##Another way using the class object:
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if self.s2:
+            return self.s2[-1]
+        else:
+            return self.s1[0]
+    def empty(self) -> bool:
+        if not self.s1 and not self.s2: 
+            return True
+        else:
+            return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()

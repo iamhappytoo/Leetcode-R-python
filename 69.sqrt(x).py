31c53e9da77a09265aa4aa2bbadec9905fc6500b
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-01-31 10:39:44 -0800

    Create 69.sqrt(x).py

---
+++ 69.sqrt(x).py
@@ -0,0 +1,27 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left = int(e**(0.5*log(x)))
+        right = left + 1
+        return left if right * right > x else right
+        
+        
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        left, right = 2, x // 2
+        
+        while left <= right:
+            pivot = left + (right - left) // 2
+            num = pivot * pivot
+            if num > x:
+                right = pivot - 1
+            elif num < x:
+                left = pivot + 1
+            else:
+                return pivot
+            
+        return right 
+                

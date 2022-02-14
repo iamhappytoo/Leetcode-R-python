621f7cdd7e653b5736dd44a3f0809324caa633a3
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-02-14 11:51:16 -0800

    Create 811. Subdomain Visit Count.py

---
+++ 811. Subdomain Visit Count.py
@@ -0,0 +1,29 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        mydict = {}
+        for string in cpdomains:
+            rep, domain = string.split()
+            domainch = domain.split('.')
+            for i in range(len(domainch)):
+                usedomain = '.'.join(domainch[i:])
+                mydict[usedomain]=mydict.get(usedomain,0)+int(rep)
+                
+        res = []
+        for key in mydict.keys():
+            res.append(' '.join([str(mydict[key]), key]))
+            
+        return res
+      
+      
+##A more brief version
+class Solution:
+  def subdomainVisits(self, cpdomains):
+    ans = collections.Counter()
+    for domain in cpdomains:
+      count,domain = domain.split()
+      count = int(count)
+      frags = domain.split('.')
+      for i in range(len(frags)):
+        ans['.'.join(frags[i:])]+=count
+       
+      return ["{} {}".format(cnt,dom) for dom, cnt in ans.items()]

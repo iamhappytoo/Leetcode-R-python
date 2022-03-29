d41fadae47a81d86d39b4f92ba18442df4cca437
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-03-29 11:30:51 -0700

    Update 9.Palindrome_Number.py

--- 9.Palindrome_Number.py
+++ 9.Palindrome_Number.py
@@ -12,4 +12,21 @@ class Solution:
             
         return True
       
+        
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        ##Edge case: 100000 or anything that ends with 0, you can find palindrome in the prefix but actually not palindrome. e.g., 1110 123210 etc.
+        if x <0 or (x % 10 == 0 and x != 0):
+            return False
+        temp = 0
+        while temp < x:
+            modu = x % 10
+            temp = temp*10 + modu
+            x = x//10
+###Edge case: odd number of digits, so 12321 123 > 21 12< 123. 
+###
+        if temp != x and x != temp//10:
+            return False
+        else:
+            return True
   

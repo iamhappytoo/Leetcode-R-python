9138e03f77cbb3489382e743fa2c145baff08a1e
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-04-04 12:27:01 -0700

    Update 14. Longest Common Prefix.py

--- 14. Longest Common Prefix.py
+++ 14. Longest Common Prefix.py
@@ -1,3 +1,4 @@
+###Solution 1: horizontal scanning
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
             
@@ -20,3 +21,17 @@ class Solution:
                 output = commpref(output, strs[pt])
                 pt += 1
             return output
+###Solution 2: Vertical scanning
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) <= 1: 
+            return strs[0]
+        if len(strs[0]) == 0:
+            return ""
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(1,len(strs)):
+                if i == len(strs[j]) or c != strs[j][i]:
+                    return strs[0][0:i]
+        return strs[0]
+    

9835af545f7716763159cbbe23475cd7ab514cab
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-04-21 11:23:18 -0700

    Update 1207. Unique Number of Occurrences.py

--- 1207. Unique Number of Occurrences.py
+++ 1207. Unique Number of Occurrences.py
@@ -12,3 +12,13 @@ class Solution:
                     mydict1[i] = 1
 
         return True
+###Another solution
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        mydict = collections.defaultdict()
+        mydict1 = collections.defaultdict()
+        for a in arr:
+            mydict[a]=mydict.get(a,0)+1
+
+        return len(list(mydict.values())) == len(set(list(mydict.values())))
+            

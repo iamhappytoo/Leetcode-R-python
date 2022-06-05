2e4338c526be328d2ce8320c98a016f17db2ec8b
ZhaoxinBan <32560090+iamhappytoo@users.noreply.github.com>
2022-06-05 14:31:21 -0700

    Update 78. Subsets.py

--- 78. Subsets.py
+++ 78. Subsets.py
@@ -29,3 +29,24 @@ class Solution:
         subsetsHelper(0)
         
         return results
+##Change the usage of local function into global function with the usage of self and the transfer of parallel self function parameters to the input
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        if not nums or len(nums) == 0:
+            return results
+        nums.sort()
+        length = len(nums)
+        subset = []
+        self.subsetsHelper(nums, 0, subset, results, length)
+        
+        return results
+    def subsetsHelper(self, nums, startIndex, subset, results, length):
+            ##deep copy subset & put in results
+            results.append(subset.copy())
+            
+            for i in range(startIndex, length):
+                subset.append(nums[i])
+                self.subsetsHelper(nums, i + 1, subset, results, length)
+                subset.pop()
+##This is much quicker than the usage of local function.                
